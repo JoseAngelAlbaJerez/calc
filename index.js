d808e5feee422e6bba6d8986c4b06e03c81e2d10
joseAngelAlbaJerez <joseangelalba24@gmail.com>
2024-11-22 11:10:03 -0400

    added ids for inputs and button and initialized them on js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+input_calc = document.getElementById('input-calc');
+square = document.getElementById('squared');
+square_root = document.getElementById('square_root');
+pi = document.getElementById('pi');
+minus = document.getElementById('minus');
+plus = document.getElementById('plus');
+division = document.getElementById('division');
+ tile_percentage = document.getElementById('%');
+
+ dot = document.getElementById('dot');
+ tile_0 = document.getElementById('0');
+ tile_1 = document.getElementById('1');
+ tile_2 = document.getElementById('2'); 
+ tile_3 = document.getElementById('3'); 
+ tile_4 = document.getElementById('4'); 
+ tile_5 = document.getElementById('5'); 
+ tile_6 = document.getElementById('6'); 
+ tile_7 = document.getElementById('7');
+ tile_8 = document.getElementById('8');
+ tile_9 = document.getElementById('9');
+
+
+
+function calctotal(params) {
+    enter = document.getElementById('enter');
+    
+    while (!enter) {
+        
+    }
+}
+
+document.getElementById('input-calc').onchange({
+
+})
